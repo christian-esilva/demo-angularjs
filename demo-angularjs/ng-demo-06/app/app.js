2315b03c2ee8fb857407417c7b69a8a9a66dba47
Christian <christian.eds@hotmail.com>
2018-09-10 22:58:39 -0300

    demo-06

---
+++ demo-angularjs/ng-demo-06/app/app.js
@@ -0,0 +1,16 @@
+﻿(function () {
+    'use strict';
+    var app = angular.module('app', ['ngAnimate', 'ngRoute']);
+
+    app.config(function ($routeProvider) {
+        $routeProvider
+            .when('/', {
+                controller: 'HomeCtrl',
+                templateUrl: 'app/areas/home/views/index.html'
+            })
+            .otherwise({
+                controller: 'HomeCtrl',
+                templateUrl: '404.html'
+            })
+    });
+})();
---
+++ demo-angularjs/ng-demo-06/app/areas/home/controllers/HomeCtrl.js
@@ -0,0 +1,13 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('HomeCtrl', HomeCtrl);
+
+    HomeCtrl.$inject = ['$scope','$location'];
+
+    function HomeCtrl($scope, $location) {
+        $scope.title = "Clientes Cadastrados";
+    }
+})();
